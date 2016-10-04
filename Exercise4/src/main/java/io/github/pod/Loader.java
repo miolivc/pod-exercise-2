01817b52e2b4d6bf45ad5d5bbc4d548f15e36443
Victor <victor.hugo.origins@gmail.com>
2016-10-04 09:18:01 -0300

    Exercise4 finished

---
+++ Exercise4/src/main/java/io/github/pod/Loader.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.pod;
+
+/**
+ *
+ * @author Victor Hugo <victor.hugo.origins@gmail.com>
+ */
+public class Loader {
+
+    static int x = 0, y = 0;
+    static Thread a, b;
+
+    public static void sum() {
+
+        x = y + 1;
+        y = x + 1;
+
+    }
+
+    public static void buildThreads() {
+
+        a = new Thread() {
+
+            @Override
+            public void run() {
+
+                sum();
+
+            }
+        };
+
+        b = new Thread() {
+
+            @Override
+            public void run() {
+
+                sum();
+
+            }
+        };
+
+    }
+    
+    public static void startThreads(){
+        
+        a.start();
+        b.start();
+        
+    }
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+
+            buildThreads();
+            
+            startThreads();
+
+            System.out.println(x);
+
+        }
+
+    }
+
+}
